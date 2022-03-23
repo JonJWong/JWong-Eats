39cf867abd4e8014b43b1ab77c0c11a69e7b9157
JonJWong <wongjonathan01@gmail.com>
2022-03-23 15:20:01 -0400

    Round up pickup CSS

--- frontend/components/map/delivery.jsx
+++ frontend/components/map/delivery.jsx
@@ -5,10 +5,51 @@ class Delivery extends React.Component {
     super(props)
   }
 
+  componentDidMount() {
+    this.props.fetchRestaurants();
+  }
+
   render() {
+    const { restaurants } = this.props;
+
     return (
       <span className="delivery-container">
+        <div className="delivery-categories">
+        categories here
+        </div>
+        <div className="delivery-banner-ads">
+        ads here
+        </div>
         <span className="delivery-title">All stores</span>
+          <div className="delivery-restaurant-list">
+              {Object.keys(restaurants).map(id => {
+                return (
+                  <div 
+                    className="delivery-restaurant-container"
+                    key={id}
+                    onClick={() => fetchRestaurant(id)}>
+                      <div className="delivery-restaurant-image"></div>
+                      <div className="delivery-restaurant-bottom">
+                        <h5 className="delivery-restaurant-name">
+                          {restaurants[id].name}
+                        </h5>
+                        <div className="delivery-restaurant-price">
+                          Price: {restaurants[id].price_rating}
+                        </div>
+                        <div className="delivery-restaurant-hours">
+                          Hours: {restaurants[id].hours}
+                        </div>
+                        <div className="delivery-restaurant-rating">
+                          {restaurants[id].rating}
+                        </div>
+                        <div className="delivery-restaurant-reviewcount">
+                          Reviews: {restaurants[id].review_count}
+                        </div>
+                      </div>
+                  </div>
+                )
+              })}
+            </div>
       </span>
     )
   }
--- frontend/components/map/pickup.jsx
+++ frontend/components/map/pickup.jsx
@@ -17,22 +17,36 @@ class Pickup extends React.Component {
       <div className="pickup-container">
         <div className="pickup-restaurants">
           <div className="pickup-title">Pickup Nearby</div>
-          {Object.keys(restaurants).map(id => {
-            return (
-              <div 
-                className="restaurant-container"
-                key={id}
-                fetchRestaurant={fetchRestaurant}>
-                  <div className="restaurant-name">
-                    {restaurants[id].name}
+            <div className="pickup-restaurant-list">
+              {Object.keys(restaurants).map(id => {
+                return (
+                  <div 
+                    className="restaurant-container"
+                    key={id}
+                    onClick={() => fetchRestaurant(id)}>
+                      <div className="restaurant-image"></div>
+                      <div className="restaurant-bottom">
+                        <h5 className="restaurant-name">
+                          {restaurants[id].name}
+                        </h5>
+                        <div className="restaurant-price">
+                          Price: {restaurants[id].price_rating}
+                        </div>
+                        <div className="restaurant-hours">
+                          Hours: {restaurants[id].hours}
+                        </div>
+                        <div className="restaurant-rating">
+                          {restaurants[id].rating}
+                        </div>
+                        <div className="restaurant-reviewcount">
+                          Reviews: {restaurants[id].review_count}
+                        </div>
+                      </div>
                   </div>
-                  <div className="restaurant-rating">
-                    {restaurants[id].rating}
-                  </div>
-              </div>
-            )
-          })}
-        </div>
+                )
+              })}
+            </div>
+          </div>
         <PickupMap />
       </div>
     )
