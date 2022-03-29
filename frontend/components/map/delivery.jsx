483a32aae8a21a05bc28aac3673ca01b3946a02f
JonJWong <wongjonathan01@gmail.com>
2022-03-29 16:42:40 -0400

    Finish sort buttons

--- frontend/components/map/delivery.jsx
+++ frontend/components/map/delivery.jsx
@@ -2,28 +2,39 @@ import React from "react";
 import { Link } from "react-router-dom";
 import * as Util from "../../util/util";
 
-const ALL_CATEGORIES = [
-  "Dine In",
-  "Take Out",
-  "Fast Food",
-  "Popular",
-  "Healthy",
-  "Italian",
-  "Mexican",
-  "Alcohol",
-  "American",
-  "Burger",
-  "Fried Chicken",
-  "Chinese",
-  "default"
+const ALL_CATEGORIES = {
+  "Dine In": "https://jwong-eats-seeds.s3.amazonaws.com/caribbean.png",
+  "Take Out": "https://jwong-eats-seeds.s3.amazonaws.com/grocery.png",
+  "Fast Food": "https://jwong-eats-seeds.s3.amazonaws.com/fastfood.png",
+  "Popular": "https://jwong-eats-seeds.s3.amazonaws.com/dessert.png",
+  "Healthy": "https://jwong-eats-seeds.s3.amazonaws.com/deli.png",
+  "Italian": "https://jwong-eats-seeds.s3.amazonaws.com/seafood.png",
+  "Mexican": "https://jwong-eats-seeds.s3.amazonaws.com/caribbean.png",
+  "Alcohol": "https://jwong-eats-seeds.s3.amazonaws.com/alcohol.png",
+  "American": "https://jwong-eats-seeds.s3.amazonaws.com/american.png",
+  "Burger": "https://jwong-eats-seeds.s3.amazonaws.com/burger.png",
+  "Fried Chicken": "https://jwong-eats-seeds.s3.amazonaws.com/korean.png",
+  "Chinese": "https://jwong-eats-seeds.s3.amazonaws.com/asian.png",
+  "default": "https://jwong-eats-seeds.s3.amazonaws.com/top_eats.png"
+}
+
+const SORT_OPTIONS = [
+  "$",
+  "$$",
+  "$$$$$",
 ]
+// "None"
 
 class Delivery extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: true
+      loading: true,
+      priceRange: []
     }
+
+    this.sort = this.sort.bind(this);
+    this.renderSortButtons = this.renderSortButtons.bind(this);
     this.renderList = this.renderList.bind(this);
     this.renderFilterMenu = this.renderFilterMenu.bind(this);
     this.ifImage = this.ifImage.bind(this);
@@ -73,7 +84,7 @@ class Delivery extends React.Component {
   renderFilterMenu() {
     return (
       <div id="delivery-categories">
-        {ALL_CATEGORIES.map(category => {
+        {Object.keys(ALL_CATEGORIES).map(category => {
           let filterName = category.split(" ").join("-")
           if (category === "default")  {
             return (
@@ -81,7 +92,17 @@ class Delivery extends React.Component {
                 className="delivery-filter-button"
                 onClick={() => this.filter()}
                 key={"default"}>
+
+                <div className="delivery-filter-icon-wrapper">
+                  <img
+                    src={ALL_CATEGORIES[category]}
+                    className="delivery-filter-icon"
+                    alt={`${category}-icon`} />
+                </div>
+
+                <div className="delivery-filter-text">
                   All Restaurants
+                </div>
               </button>
             )
           } else {
@@ -90,7 +111,17 @@ class Delivery extends React.Component {
                 className="delivery-filter-button"
                 onClick={() => this.filter(filterName)}
                 key={filterName}>
-                  {category}
+
+                <div className="delivery-filter-icon-wrapper">
+                  <img
+                    src={ALL_CATEGORIES[category]}
+                    className="delivery-filter-icon"
+                    alt={`${category}-icon`} />
+                </div>
+
+                  <div className="delivery-filter-text">
+                    {category}
+                  </div>
               </button>
             )
           }
@@ -99,16 +130,83 @@ class Delivery extends React.Component {
     )
   }
 
-  sort(option) {
+  sort(restaurants) {
+    let options;
+    if (this.state.priceRange.length > 0) {
+      options = this.state.priceRange
+    }
 
+    const newRestaurants = {};
+    
+    Object.keys(restaurants).map(id => {
+      let restaurant = restaurants[id];
+      if (options?.includes(restaurant.price_rating)) {
+        newRestaurants[id] = restaurant
+      }
+    })
+    
+    return newRestaurants;
   }
 
-  renderSorted(restaurants) {
+  togglePriceRange(option, e) {
+    e.currentTarget.classList.toggle('delivery-sort-button-selected');
+    const newState = this.state;
+    if (!newState.priceRange.some(ele => ele === option)
+      && option !== "None") {
+        newState.priceRange.push(option);
+    } else if (option === "None") {
+      newState.priceRange = [];
+    } else {
+      newState.priceRange = 
+        newState.priceRange.filter(ele => ele !== option);
+    }
+    this.setState(newState);
+  }
 
+  renderSortButtons() {
+    return (
+      <div id="delivery-left">
+        <div className="delivery-left-header">
+          Price Range
+        </div>
+        <div className="sort-button-wrapper">
+          {SORT_OPTIONS.map(option => {
+            if (option === "None") {
+              return (
+                <button
+                  key={option}
+                  className="delivery-sort-button"
+                  onClick={(e) => this.togglePriceRange(option, e)}>
+                    <div className="sort-option-text">
+                      None
+                    </div>
+                </button>
+              )
+            } else {
+              return (
+                <button
+                key={option}
+                className="delivery-sort-button"
+                onClick={(e) => this.togglePriceRange(option, e)}>
+                  <div className="sort-option-text">
+                    {option}
+                  </div>
+                </button>
+              )
+            }
+          })}
+        </div>
+      </div>
+    )
   }
 
   renderList() {
-    const { restaurants } = this.state;
+    let { restaurants } = this.state;
+
+    if (this.state.priceRange.length > 0) {
+      restaurants = this.sort(restaurants)
+    }
+
     return (
       <div id="delivery-restaurant-list">
       {Object.keys(restaurants).map(id => {
@@ -147,7 +245,7 @@ class Delivery extends React.Component {
   }
 
   render() {
-    const { restaurants, loading } = this.state;
+    const { loading } = this.state;
 
     if (loading) {
       return (
@@ -171,7 +269,7 @@ class Delivery extends React.Component {
           <h1 id="delivery-top-picks">Top picks for you:</h1>
         </div>
 
-        <div className="delivery-left"></div>
+        {this.renderSortButtons()}
 
         {this.renderList()}
       </div>
