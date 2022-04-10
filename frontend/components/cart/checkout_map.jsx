3747a859ea183393a9afed2a658c32f2de6c15fb
JonJWong <wongjonathan01@gmail.com>
2022-04-09 21:04:22 -0400

    Update map with dark mode, restriction

--- frontend/components/cart/checkout_map.jsx
+++ frontend/components/cart/checkout_map.jsx
@@ -1,128 +1,270 @@
 import React from "react";
 
+// Circle marker params
+const LIGHT_CIRCLE = {
+  path: google.maps.SymbolPath.CIRCLE,
+  scale: 15,
+  fillColor: "#FFFFFF",
+  strokeColor: "#FFFFFF",
+  fillOpacity: 1.0,
+  strokeWeight: 0.4
+}
+
+// Dark circle marker params
+const DARK_CIRCLE = {
+  path: google.maps.SymbolPath.CIRCLE,
+  scale: 15,
+  fillColor: "#000000",
+  strokeColor: "#000000",
+  fillOpacity: 1.0,
+  strokeWeight: 0.4
+}
+
+const CENTER = { lat: 40.6962131, lng: -74.302344 } // Union, NJ
+
 // Map options for controls
 const MAP_OPTIONS = {
-  center: { lat: 40.6962131, lng: -74.302344 }, // Union, NJ
+  center: CENTER,
   zoom: 15,
   streetViewControl: false,
   mapTypeControl: false,
-  fullscreenControl: false
+  fullscreenControl: false,
+  keyboardShortcuts: false,
+  backgroundColor: "none",
+  gestureHandling: "none",
+  minZoom: 14,
+  maxZoom: 17,
+  restriction: {
+    latLngBounds: {
+      north: CENTER.lat + .03,
+      south: CENTER.lat - .03,
+      east: CENTER.lng + .07,
+      west: CENTER.lng - .07
+    }
+  }
 };
 
-// Map styling to hide default pois
+// Maps query params
+const REQUEST = {
+  location: MAP_OPTIONS.center,
+  radius: 5000, // meters
+  type: ['restaurant']
+}
+
+// Styles constant for night mode and hiding default POIs
 const STYLES = {
-  default: [],
-  hide: [
+  default: [
     {
-      "featureType": "administrative",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.business",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.business",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.government",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.medical",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.park",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.place_of_worship",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.school",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.sports_complex",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "road.local",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "transit",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
     }
-  ]
+  ],
+  dark: [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
+    }
+  ],
+  hiding: [
+    {
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
 };
 
 class CheckoutMap extends React.Component {
@@ -134,7 +276,17 @@ class CheckoutMap extends React.Component {
 
   componentDidMount() {
     this.map = new google.maps.Map(this.mapNode, MAP_OPTIONS);
-    this.map.setOptions({ styles: STYLES["hide"] });
+
+    // get time of day and set a styles var accordingly
+    const hour = new Date().getHours();
+    let styles;
+    if (hour < 7 || hour > 17) {
+      styles = STYLES["dark"]
+    } else {
+      styles = STYLES["default"]
+    }
+    // apply styles by time of day
+    this.map.setOptions({ styles: styles });
   }
 
   render() {
--- frontend/components/map/pickup_map.jsx
+++ frontend/components/map/pickup_map.jsx
@@ -21,13 +21,27 @@ const DARK_CIRCLE = {
   strokeWeight: 0.4
 }
 
+const CENTER = { lat: 40.6962131, lng: -74.302344 } // Union, NJ
+
 // Map options for controls
 const MAP_OPTIONS = {
-  center: { lat: 40.6962131, lng: -74.302344 }, // Union, NJ
+  center: CENTER,
   zoom: 15,
   streetViewControl: false,
   mapTypeControl: false,
-  fullscreenControl: false
+  fullscreenControl: false,
+  keyboardShortcuts: false,
+  backgroundColor: "none",
+  minZoom: 14,
+  maxZoom: 17,
+  restriction: {
+    latLngBounds: {
+      north: CENTER.lat + .03,
+      south: CENTER.lat - .03,
+      east: CENTER.lng + .07,
+      west: CENTER.lng - .07
+    }
+  }
 };
 
 // Maps query params
@@ -37,120 +51,220 @@ const REQUEST = {
   type: ['restaurant']
 }
 
-// Map styling to hide default pois
+// Styles constant for night mode and hiding default POIs
 const STYLES = {
-  default: [],
-  hide: [
+  default: [
     {
-      "featureType": "administrative",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "administrative.land_parcel",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.business",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.business",
-      "elementType": "geometry",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.government",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.medical",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.park",
-      "elementType": "labels.text",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.place_of_worship",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.school",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "poi.sports_complex",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "road.local",
-      "elementType": "labels",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
     },
     {
-      "featureType": "transit",
-      "stylers": [
-        {
-          "visibility": "off"
-        }
-      ]
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
     }
-  ]
+  ],
+  dark: [
+    { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
+    {
+      featureType: "administrative.locality",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "geometry",
+      stylers: [{ color: "#263c3f" }],
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#6b9a76" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [{ color: "#38414e" }],
+    },
+    {
+      featureType: "road",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#212a37" }],
+    },
+    {
+      featureType: "road",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#9ca5b3" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [{ color: "#746855" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "geometry.stroke",
+      stylers: [{ color: "#1f2835" }],
+    },
+    {
+      featureType: "road.highway",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#f3d19c" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "geometry",
+      stylers: [{ color: "#2f3948" }],
+    },
+    {
+      featureType: "transit.station",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#d59563" }],
+    },
+    {
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#515c6d" }],
+    },
+    {
+      featureType: "water",
+      elementType: "labels.text.stroke",
+      stylers: [{ color: "#17263c" }],
+    },
+    {
+      featureType: "administrative",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "administrative.land_parcel",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.business",
+      elementType: "geometry",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.government",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.medical",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.park",
+      elementType: "labels.text",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.place_of_worship",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.school",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "poi.sports_complex",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "road.local",
+      elementType: "labels",
+      stylers: [{visibility: "off"}]
+    },
+    {
+      featureType: "transit",
+      stylers: [{visibility: "off"}]
+    }
+  ],
+  hiding: [
+    {
+      featureType: "administrative",
+      elementType: "labels",
+      stylers: [{ visibility: "off" }]
+    },
+    {
+      featureType: "poi.business",
+      stylers: [{ visibility: "off" }],
+    },
+    {
+      featureType: "transit",
+      elementType: "labels.icon",
+      stylers: [{ visibility: "off" }],
+    },
+  ],
 };
 
 class PickupMap extends React.Component{
@@ -263,12 +377,27 @@ class PickupMap extends React.Component{
     this.props.fetchRestaurants();
     // Wrap this.mapNode in a Google Map
     this.map = new google.maps.Map(this.mapNode, MAP_OPTIONS);
-    this.map.setOptions({ styles: STYLES["hide"] });
+
+    // get time of day and set a styles var accordingly
+    const hour = new Date().getHours();
+    let styles;
+    if (hour < 7 || hour > 17) {
+      styles = STYLES["dark"]
+    } else {
+      styles = STYLES["default"]
+    }
+    // apply styles by time of day
+    this.map.setOptions({ styles: styles });
+
     // Initialize places service
     this.service = new google.maps.places.PlacesService(this.map);
 
     // Populate map with markers
     this.service.nearbySearch(REQUEST, this.mapCallback);
+
+    google.maps.event.addListener(this.map, "click", () => {
+      this.closeInfoWindows();
+    })
   }
 
   // Remove map, service, attributes when component unmounts
