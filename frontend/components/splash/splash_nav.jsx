6e32f0da3493d60c48834beb54d9fb5b7518f564
JonJWong <wongjonathan01@gmail.com>
2022-08-16 16:03:36 -0400

    Overhaul Splash+Nav CSS

--- frontend/components/splash/splash_nav.jsx
+++ frontend/components/splash/splash_nav.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import UserModalContainer from '../user_modal/user_modal_container';
+import React from "react";
+import { Link } from "react-router-dom";
+import UserModalContainer from "../user_modal/user_modal_container";
 
 class SplashNav extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       modalOpen: false,
-    }
+    };
     this.toggleModal = this.toggleModal.bind(this);
     this.renderModal = this.renderModal.bind(this);
   }
@@ -15,43 +15,36 @@ class SplashNav extends React.Component {
   // Helper to open/close side modal
   toggleModal() {
     let setValue = !this.state.modalOpen;
-    this.setState({ modalOpen: setValue })
+    this.setState({ modalOpen: setValue });
   }
 
   // Eender modal if state is open
   renderModal() {
     if (this.state.modalOpen) {
-      return <UserModalContainer toggleModal={this.toggleModal} />
+      return <UserModalContainer toggleModal={this.toggleModal} />;
     }
   }
 
   render() {
     return (
-      <nav id="splash-nav-bar">
-
-        <div id="splash-nav-contents">
-          <div id="splash-left">
-            <button
-              onClick={() => this.toggleModal()}
-              id="splash-hamburger">
-                <i className="fas fa-bars fa-xl"></i>
+      <nav className="splash-nav">
+        <section>
+          <div className="left">
+            <button onClick={() => this.toggleModal()}>
+              <i className="fas fa-bars fa-xl"></i>
             </button>
 
-            <div id="splash-eats-logo" />
+            <div className="splash-logo" />
           </div>
 
-          <div id="splash-right">
-            <Link
-              to="/login"
-              id="splash-nav-login">
-                Sign In
-            </Link>
+          <div className="right">
+            <Link to="/login">Sign In</Link>
           </div>
-        </div>
-        
+        </section>
+
         {this.renderModal()}
       </nav>
-    )
+    );
   }
 }
 
