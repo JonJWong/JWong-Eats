c983461d40539a1980bf4dae229f41404a9ccaf7
JonJWong <wongjonathan01@gmail.com>
2023-02-01 21:20:56 -0500

    Util refactor

--- frontend/components/map/pickup.jsx
+++ frontend/components/map/pickup.jsx
@@ -5,14 +5,13 @@ import * as Util from "../../util/util";
 
 class Pickup extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
   }
 
   componentDidMount() {
-    this.props.fetchRestaurants()
-    document.title = "Order Food Online | JWongEats"
+    this.props.fetchRestaurants();
+    document.title = "Order Food Online | JWongEats";
   }
-
 
   render() {
     const { restaurants } = this.props;
@@ -20,51 +19,47 @@ class Pickup extends React.Component {
     return (
       <div id="pickup-container">
         <div id="pickup-restaurants">
-          
           <div id="pickup-title">Pickup Nearby</div>
-            <div id="pickup-restaurant-list">
-              {Object.keys(restaurants).map(id => {
-                return (
-                  <Link
-                    key={id}
-                    to={`/restaurants/${id}`}>
-                      
-                    <div 
-                      className="restaurant-container"
-                      key={id}>
-                      <img src={restaurants[id].photoUrl}
-                        className="restaurant-image" />
+          <div id="pickup-restaurant-list">
+            {Object.keys(restaurants).map((id) => {
+              return (
+                <Link key={id} to={`/restaurants/${id}`}>
+                  <div className="restaurant-container" key={id}>
+                    <img
+                      src={restaurants[id].photoUrl}
+                      className="restaurant-image"
+                    />
 
-                      <div className="restaurant-bottom">
-                        <h5 className="restaurant-name">
-                          {restaurants[id].name}
-                        </h5>
+                    <div className="restaurant-bottom">
+                      <h5 className="restaurant-name">
+                        {restaurants[id].name}
+                      </h5>
 
-                        <div className="restaurant-price">
-                          Price: {restaurants[id].price_rating}
-                        </div>
+                      <div className="restaurant-price">
+                        Price: {restaurants[id].price_rating}
+                      </div>
 
-                        <div className="restaurant-hours">
-                          {Util.timeDifferencePrompt(restaurants[id].hours)}
-                        </div>
+                      <div className="restaurant-hours">
+                        {Util.timeDifferencePrompt(restaurants[id].hours)}
+                      </div>
 
-                        <div className="restaurant-rating">
-                          {Util.addZero(restaurants[id].rating)}
-                        </div>
+                      <div className="restaurant-rating">
+                        {restaurants[id].rating.toFixed(1)}
+                      </div>
 
-                        <div className="restaurant-reviewcount">
-                          Reviews: {restaurants[id].review_count}
-                        </div>
+                      <div className="restaurant-reviewcount">
+                        Reviews: {restaurants[id].review_count}
                       </div>
                     </div>
-                  </Link>
-                )
-              })}
-            </div>
+                  </div>
+                </Link>
+              );
+            })}
           </div>
+        </div>
         <PickupMap />
       </div>
-    )
+    );
   }
 }
 
