57c197e7b932e87cbdd2c940934330ca1b44e341
JonJWong <wongjonathan01@gmail.com>
2023-03-07 07:19:40 -0500

    user_modal refactor

--- frontend/components/user_modal/user_modal.jsx
+++ frontend/components/user_modal/user_modal.jsx
@@ -6,38 +6,30 @@ class UserModal extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tabOpen: false
-    }
+      tabOpen: false,
+    };
 
     this.closeAndRemove = this.closeAndRemove.bind(this);
     this.toggleOpen = this.toggleOpen.bind(this);
     this.toggleTab = this.toggleTab.bind(this);
-    this.rendertab = this.renderTab.bind(this);
   }
-  
+
   // Helper method to toggle modal state
   toggleTab() {
     const setValue = !this.state.tabOpen;
     this.setState({ tabOpen: setValue });
   }
 
-  // Render modal if state is open
-  renderTab() {
-    if (this.state.tabOpen) {
-      return <UserAccountContainer />
-    }
-  }
-
   componentDidMount() {
     setTimeout(() => {
       this.toggleOpen();
-    }, 10)
+    }, 10);
   }
 
   // Helper method to add/remove transition effect class
   toggleOpen() {
-    const modal = document.querySelector('.modal-contents');
-    modal.classList.toggle('modal-open')
+    const modal = document.querySelector(".modal-contents");
+    modal.classList.toggle("modal-open");
   }
 
   // Helper method to trigger transition and "unmount" modal
@@ -46,7 +38,7 @@ class UserModal extends React.Component {
     this.toggleOpen();
     setTimeout(() => {
       toggleModal("modalOpen");
-    }, 510)
+    }, 510);
   }
 
   render() {
@@ -57,93 +49,84 @@ class UserModal extends React.Component {
       return (
         <div id="user-modal">
           <div className="modal-contents">
-
             <div className="user-info">
               <span className="profile-photo" />
-              <span className="profile-name">
-                {currentUser.first_name}
-              </span>
+              <span className="profile-name">{currentUser.first_name}</span>
 
-              <button 
+              <button
                 onClick={() => this.toggleTab()}
-                id={open ? "close-account-button" :"view-account-button"}>
-                  {open ? "Close" : "View Account"}
+                id={open ? "close-account-button" : "view-account-button"}
+              >
+                {open ? "Close" : "View Account"}
               </button>
             </div>
 
-            <Link
-              to={`/orderhistory/${currentUser.id}`}
-              id="orders-link">
-                <div
-                  className="orders-link-container"
-                  onClick={() => this.closeAndRemove()}>
-                  <i className="fas fa-receipt"></i> Orders
-                </div>
+            <Link to={`/orderhistory/${currentUser.id}`} id="orders-link">
+              <div
+                className="orders-link-container"
+                onClick={() => this.closeAndRemove()}
+              >
+                <i className="fas fa-receipt"></i> Orders
+              </div>
             </Link>
 
             <div id="modal-signout-container">
-              <button
-                onClick={() => logout()}
-                id="modal-signout">
-                  Sign Out
+              <button onClick={() => logout()} id="modal-signout">
+                Sign Out
               </button>
             </div>
           </div>
 
-          <div id="user-modal-block" 
-            onClick={() => this.closeAndRemove()} />
+          <div id="user-modal-block" onClick={() => this.closeAndRemove()} />
         </div>
-      )
+      );
     } else if (currentUser && open) {
       return (
         <div id="user-modal">
           <div className="modal-contents">
-
-            {this.renderTab()}
+            {this.state.tabOpen && <UserAccountContainer />}
 
             <button
               onClick={() => this.toggleTab()}
-              id={open ? "close-account-button" :"view-account-button"}>
-                {open ? "Close" : "View Account"}
+              id={open ? "close-account-button" : "view-account-button"}
+            >
+              {open ? "Close" : "View Account"}
             </button>
 
             <div id="modal-signout-container">
-              <button
-                onClick={() => logout()}
-                id="modal-signout">
-                  Sign Out
+              <button onClick={() => logout()} id="modal-signout">
+                Sign Out
               </button>
             </div>
           </div>
 
-          <div id="user-modal-block"
-            onClick={() => this.closeAndRemove()} />
+          <div id="user-modal-block" onClick={() => this.closeAndRemove()} />
         </div>
-      )
+      );
     } else {
       return (
         <div id="splash-user-modal">
           <div className="modal-contents">
             <Link to="/login">
-              <div id="splash-modal-login">
-                Sign In
-              </div>
+              <div id="splash-modal-login">Sign In</div>
             </Link>
 
             <button
-              onClick={()=>login({
-                email: 'demo_user@email.com',
-                password: 'password'
-              })}
-              id="splash-modal-demo">
-                Demo Login
+              onClick={() =>
+                login({
+                  email: "demo_user@email.com",
+                  password: "password",
+                })
+              }
+              id="splash-modal-demo"
+            >
+              Demo Login
             </button>
           </div>
 
-          <div id="user-modal-block"
-            onClick={() => this.closeAndRemove()} />
+          <div id="user-modal-block" onClick={() => this.closeAndRemove()} />
         </div>
-      )
+      );
     }
   }
 }
